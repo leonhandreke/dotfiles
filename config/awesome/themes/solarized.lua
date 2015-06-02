cce5c290d4db312559527c5c9016ac5ae33f1382
Leon Handreke <leonh@ndreke.de>
2015-06-02 18:49:38 +0200

    Remove awesome config

--- config/awesome/themes/solarized.lua
+++
@@ -1,5 +0,0 @@
--- Hacky script to set the font on the solarized dark theme object
-local awful = require("awful")
-theme = dofile(awful.util.getdir("config") .. "/themes/awesome-solarized/dark/theme.lua")
-theme.font = "sans 8"
-return theme
